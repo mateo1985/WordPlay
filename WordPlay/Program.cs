5e55623d01e63972c19aa476d4791a7876a5e89a
MVPB63 <MVPB63@MVPB63-03>
2018-04-06 11:06:21 +0200

    Show result

--- WordPlay/Program.cs
+++ WordPlay/Program.cs
@@ -2,6 +2,7 @@
 {
     using System.Collections.Generic;
     using WordPlay.Operations;
+    using System;
     using WordPlay.Transformations;
 
     
@@ -11,6 +12,8 @@
         static void Main(string[] args)
         {
             var exmapleString = "Tests of [a ([(sample test1)] supplied)] by MeMFIS [Test1 Test2 (test3 test4)]";
+            Console.WriteLine($"The input string before conversion:{exmapleString}");
+
 
             var operationManager = new OperationsManager();
             operationManager.RegisterOperation(new ReverseOperation());
@@ -19,7 +22,8 @@
             var test = new TransformString(operationManager);
 
             var result = test.TransfromString(exmapleString);
-
+            Console.WriteLine($"The output string after conversion:{result}");
+            Console.ReadLine();
         }
         
         
